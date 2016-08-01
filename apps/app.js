e590a90195341e4631c77ba88bf33fa526174685
Dan Jenner <danjenner@gmail.com>
2016-08-01 15:39:16 -0500

    Youtube AJAX api completed

--- apps/app.js
+++ apps/app.js
@@ -1,28 +1,43 @@
-function getResults(searchTerm) {
-    var params = {
-        s: searchTerm,
-        r: 'json'
-    };
-    url = 'http://www.omdbapi.com';
-
-    $.getJSON(url, params, function (data) {
-        showResults(data.Search);
-    });
-}
-
-
 $(document).ready(function () {
     $('#search-term').submit(function (event) {
         event.preventDefault();
         var searchTerm = $('#query').val();
         getResults(searchTerm);
-
     });
 })
 
-function showResults(results) {
-    $.each(results, function (index, value) {
-        $('div.results').append("<P>" + value.Title + "</P>");
-    });
+function getResults(searchTerm) {
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyAWhM-E86yQpMoyxugp4mztxAxZ0xF3FWU',
+        q: searchTerm
+    };
+    url = 'https://www.googleapis.com/youtube/v3/search';
 
+    $.getJSON(url, params, function (data) {
+        showResults(data);
+    });
+}
+
+function showResults(data) {
+    $('div.results').empty();
+    for (i = 0; i < data.items.length; i++) {
+
+        var title = data.items[i].snippet.title;
+        var id = data.items[i].id.videoId;
+        var thumbnail = data.items[i].snippet.thumbnails.medium.url;
+        var channel = data.items[i].snippet.channelId;
+        if (data.items[i].id.videoId) {
+            id = data.items[i].id.videoId;
+        } else {
+            id = "No video";
+        }
+
+
+        var content = "<p><a href=\"https://www.youtube.com/watch?v=" + id + "\"\>" + "<img src=\"" + thumbnail + "\"></a></p>";
+        $('div.results').append(content);
+        var channelLink = "<a href=\"https://www.youtube.com/channel/" + channel + "\">Watch Channel</a>"
+        $('div.results').append(channelLink);
+        console.log(channelLink)
+    }
 }
