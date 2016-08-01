46c372ceffae8a078368e1f8d4c9f9f42a7e9de0
Dan Jenner <danjenner@gmail.com>
2016-08-01 11:40:00 -0500

    Working demo

--- apps/app.js
+++ apps/app.js
@@ -1,29 +1,28 @@
-function showResults(myData) {
-    $.each(myData, function (key, value) {
-        $('div.results').append("<P>" + value.Title + "</P>");
-    });
-}
-
-function getJson(searchTerm) {
+function getResults(searchTerm) {
     var params = {
         s: searchTerm,
-        r: 'json',
+        r: 'json'
     };
-    url = 'http//www.omdbapi.com/';
+    url = 'http://www.omdbapi.com';
 
     $.getJSON(url, params, function (data) {
-        var myData = data.Search;
-        showResults(myData);
+        showResults(data.Search);
     });
 }
 
-$(document).ready(function () {
 
+$(function () {
     $('#search-term').submit(function (event) {
-        event.preventDefault(event);
-        console.log('test');
+        event.preventDefault();
         var searchTerm = $('#query').val();
-        console.log(searchTerm);
-        getJson(searchTerm);
+        getResults(searchTerm);
+
     });
-});
+})
+
+function showResults(results) {
+    $.each(results, function (index, value) {
+        $('div.results').append("<P>" + value.Title + "</P>");
+    });
+
+}
